54033bb83b57eb88c35e57559e32429d8a6b9d20
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-26 16:04:09 +0800

    Update addBatch.vue

--- farmAdmin/pages/system/permission/addBatch.vue
+++ farmAdmin/pages/system/permission/addBatch.vue
@@ -1,98 +1,95 @@
 <template>
-  <view class="uni-container">
-    <uni-forms ref="form" :value="formData" validateTrigger="bind">
-      <uni-forms-item name="permission_id" label="权限ID" required>
-        <input placeholder="权限唯一标识，不可修改，不允许重复" @input="binddata('permission_id', $event.detail.value)" class="uni-input-border" v-model="formData.permission_id" trim="both" />
-      </uni-forms-item>
-      <uni-forms-item name="permission_name" label="权限名称" required>
-        <input placeholder="权限名称" @input="binddata('permission_name', $event.detail.value)" class="uni-input-border" v-model="formData.permission_name" trim="both" />
-      </uni-forms-item>
-      <uni-forms-item name="comment" label="备注">
-        <textarea placeholder="备注" @input="binddata('comment', $event.detail.value)" class="uni-textarea-border" v-model="formData.comment" trim="both" />
-      </uni-forms-item>
-      <view class="uni-button-group">
-        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">{{$t('common.button.submit')}}</button>
-        <navigator open-type="navigateBack" style="margin-left: 15px;">
-            <button class="uni-button" style="width: 100px;">{{$t('common.button.back')}}</button>
-        </navigator>
-      </view>
-    </uni-forms>
-  </view>
+	<view class="uni-container">
+		<uni-forms ref="form" :value="formData" validateTrigger="bind" :label-width="100">
+			<uni-forms-item name="prefix" label="权限ID前缀" required>
+				<input placeholder="权限唯一标识，不可修改，不允许重复" @input="binddata('prefix', $event.detail.value)"
+					class="uni-input-border" v-model="formData.prefix" trim="both" />
+			</uni-forms-item>
+			<uni-forms-item name="name" label="权限名称" required>
+				<input placeholder="权限名称" @input="binddata('name', $event.detail.value)" class="uni-input-border"
+					v-model="formData.name" trim="both" />
+			</uni-forms-item>
+			<view class="uni-button-group">
+				<button type="primary" class="uni-button" style="width: 100px;"
+					@click="submit">{{$t('common.button.submit')}}</button>
+				<navigator open-type="navigateBack" style="margin-left: 15px;">
+					<button class="uni-button" style="width: 100px;">{{$t('common.button.back')}}</button>
+				</navigator>
+			</view>
+		</uni-forms>
+	</view>
 </template>
 
 <script>
-  import { validator } from '@/js_sdk/validator/uni-id-permissions.js';
+	const db = uniCloud.database();
+	const dbCmd = db.command;
+	const dbCollectionName = 'uni-id-permissions';
 
-  const db = uniCloud.database();
-  const dbCmd = db.command;
-  const dbCollectionName = 'uni-id-permissions';
+	export default {
+		data() {
+			return {
+				formData: {
+					"prefix": "",
+					"name": ""
+				}
+			}
+		},
+		methods: {
+			submit() {
+				if (!this.formData.prefix) {
+					uni.showToast({
+						title: '请添加权限ID前缀',
+						icon:'none'
+					})
+					uni.showto
+					return
+				}
 
-  function getValidator(fields) {
-    let result = {}
-    for (let key in validator) {
-      if (fields.includes(key)) {
-        result[key] = validator[key]
-      }
-    }
-    return result
-  }
+				if (!this.formData.name) {
+					uni.showToast({
+						title: '请添加权限名称',
+						icon:'none'
+					})
+					return
+				}
+				const value = [
+					{
+						"permission_id": this.formData.prefix + "_create",
+						"permission_name": "创建" + this.formData.name,
+					},
+					{
+						"permission_id": this.formData.prefix + "_read",
+						"permission_name": "读取" + this.formData.name,
+					},
+					{
+						"permission_id": this.formData.prefix + "_emit",
+						"permission_name": "编辑" + this.formData.name,
+					},
+					{
+						"permission_id": this.formData.prefix + "_delete",
+						"permission_name": "删除" + this.formData.name,
+					}
+				]
+				this.submitForm(value)
+			},
 
-  export default {
-    data() {
-      let formData = {
-        "permission_id": "",
-        "permission_name": "",
-        "comment": ""
-      }
-      return {
-        formData,
-        formOptions: {},
-        rules: {
-          ...getValidator(Object.keys(formData))
-        }
-      }
-    },
-    onReady() {
-      this.$refs.form.setRules(this.rules)
-    },
-    methods: {
-      /**
-       * 触发表单提交
-       */
-      submit() {
-        uni.showLoading({
-          mask: true
-        })
-        this.$refs.form.validate().then((res) => {
-          this.submitForm(res)
-        }).catch(() => {
-          uni.hideLoading()
-        })
-      },
-
-      submitForm(value) {
-        // 使用 clientDB 提交数据
-        db.collection(dbCollectionName).add(value).then((res) => {
-          uni.showToast({
-            title: '新增成功'
-          })
-          this.getOpenerEventChannel().emit('refreshData')
-          setTimeout(() => uni.navigateBack(), 500)
-        }).catch((err) => {
-          uni.showModal({
-            content: err.message || '请求服务失败',
-            showCancel: false
-          })
-        }).finally(() => {
-          uni.hideLoading()
-        })
-      }
-    }
-  }
-</script>
-
-<style>
-	::v-deep .uni-forms-item__label {
-		width: 90px !important;
+			submitForm(value) {
+				// 使用 clientDB 提交数据
+				db.collection(dbCollectionName).add(value).then((res) => {
+					uni.showToast({
+						title: '新增成功'
+					})
+					this.getOpenerEventChannel().emit('refreshData')
+					setTimeout(() => uni.navigateBack(), 500)
+				}).catch((err) => {
+					uni.showModal({
+						content: err.message || '请求服务失败',
+						showCancel: false
+					})
+				}).finally(() => {
+					uni.hideLoading()
+				})
+			}
+		}
 	}
-</style>
+</script>
