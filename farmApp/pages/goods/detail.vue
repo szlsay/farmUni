7d658e33b8d3c7f9b0a949e8b93333e25512158e
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-27 13:59:20 +0800

    Update detail.vue

--- farmApp/pages/goods/detail.vue
+++ farmApp/pages/goods/detail.vue
@@ -1,150 +1,102 @@
 <template>
-  <view class="container">
-    <unicloud-db ref="udb" v-slot:default="{data, loading, error, options}" :options="options" :collection="collectionList" field="goods_sn,name,keywords,goods_desc,goods_thumb,remain_count,month_sell_count,total_sell_count,comment_count,is_on_sale,is_hot,add_date,last_modify_date" :where="queryWhere" :getone="true" :manual="true">
-      <view v-if="error">{{error.message}}</view>
-      <view v-else-if="loading">
-        <uni-load-more :contentText="loadMore" status="loading"></uni-load-more>
-      </view>
-      <view v-else-if="data">
-        <view>
-          <text>货号</text>
-          <text>{{data.goods_sn}}</text>
-        </view>
-        <view>
-          <text>名称</text>
-          <text>{{data.name}}</text>
-        </view>
-        <view>
-          <text>关键字</text>
-          <text>{{data.keywords}}</text>
-        </view>
-        <view>
-          <text>详细描述</text>
-          <text>{{data.goods_desc}}</text>
-        </view>
-        <view>
-          <text>商品图片</text>
-          <uni-file-picker v-if="data.goods_thumb && data.goods_thumb.fileType == 'image'" :value="data.goods_thumb" :file-mediatype="data.goods_thumb && data.goods_thumb.fileType" return-type="object" readonly></uni-file-picker>
-          <uni-link v-else-if="data.goods_thumb" :href="data.goods_thumb.url" :text="data.goods_thumb.url"></uni-link>
-          <text v-else></text>
-        </view>
-        <view>
-          <text>库存数量</text>
-          <text>{{data.remain_count}}</text>
-        </view>
-        <view>
-          <text>月销量</text>
-          <text>{{data.month_sell_count}}</text>
-        </view>
-        <view>
-          <text>总销量</text>
-          <text>{{data.total_sell_count}}</text>
-        </view>
-        <view>
-          <text>评论数</text>
-          <text>{{data.comment_count}}</text>
-        </view>
-        <view>
-          <text>是否上架</text>
-          <text>{{data.is_on_sale == true ? '✅' : '❌'}}</text>
-        </view>
-        <view>
-          <text>是否热销</text>
-          <text>{{data.is_hot == true ? '✅' : '❌'}}</text>
-        </view>
-        <view>
-          <text>上架时间</text>
-          <uni-dateformat :threshold="[0, 0]" :date="data.add_date"></uni-dateformat>
-        </view>
-        <view>
-          <text>最后修改时间</text>
-          <uni-dateformat :threshold="[0, 0]" :date="data.last_modify_date"></uni-dateformat>
-        </view>
-      </view>
-    </unicloud-db>
-    <view class="btns">
-      <button type="primary" @click="handleUpdate">修改</button>
-      <button type="warn" class="btn-delete" @click="handleDelete">删除</button>
-    </view>
-  </view>
+	<view class="container">
+		<unicloud-db ref="udb" v-slot:default="{data, loading, error, options}" :options="options"
+			:collection="collectionList" field="name,goods_desc,goods_thumb,month_sell_count,comment_count,is_hot"
+			:where="queryWhere" :getone="true" :manual="true">
+			<view v-if="error">{{error.message}}</view>
+			<view v-else-if="loading">
+				<uni-load-more :contentText="loadMore" status="loading"></uni-load-more>
+			</view>
+			<view v-else-if="data">
+				<view>
+					<image :src="data.goods_thumb.path" :mode="aspectFill" style="height: 500rpx;"/>
+				</view>
+				<view class=""></view>
+				<view>
+					<text>名称:{{data.name}}</text>
+				</view>
+				<view>
+					<text>详细描述:{{data.goods_desc}}</text>
+				</view>
+				<view>
+					<text>{{data.is_hot == true ? '✅' : '❌'}}</text>
+					<text style="margin-left: 16rpx;">月销量:{{data.month_sell_count}}</text>
+					<text style="margin-left: 16rpx;">评论数:{{data.comment_count}}</text>
+				</view>
+			</view>
+		</unicloud-db>
+		<view class="btns">
+			<button type="primary" @click="handleCart">加入购物车</button>
+			<button type="warn" class="btn-delete" @click="handleBuy">立即购买</button>
+		</view>
+	</view>
 </template>
 
 <script>
-  // 由schema2code生成，包含校验规则和enum静态数据
-  import { enumConverter } from '../../js_sdk/validator/goods.js'
-  const db = uniCloud.database()
+	import {
+		enumConverter
+	} from '../../js_sdk/validator/goods.js'
+	const db = uniCloud.database()
 
-  export default {
-    data() {
-      return {
-        queryWhere: '',
-        collectionList: "goods",
-        loadMore: {
-          contentdown: '',
-          contentrefresh: '',
-          contentnomore: ''
-        },
-        options: {
-          // 将scheme enum 属性静态数据中的value转成text
-          ...enumConverter
-        }
-      }
-    },
-    onLoad(e) {
-      this._id = e.id
-    },
-    onReady() {
-      if (this._id) {
-        this.queryWhere = '_id=="' + this._id + '"'
-      }
-    },
-    methods: {
-      handleUpdate() {
-        // 打开修改页面
-        uni.navigateTo({
-          url: './edit?id=' + this._id,
-          events: {
-            // 监听修改页面成功修改数据后, 刷新当前页面数据
-            refreshData: () => {
-              this.$refs.udb.loadData({
-                clear: true
-              })
-            }
-          }
-        })
-      },
-      handleDelete() {
-        this.$refs.udb.remove(this._id, {
-          success: (res) => {
-            // 删除数据成功后跳转到list页面
-            uni.navigateTo({
-              url: './list'
-            })
-          }
-        })
-      }
-    }
-  }
+	export default {
+		data() {
+			return {
+				queryWhere: '',
+				collectionList: "goods",
+				loadMore: {
+					contentdown: '',
+					contentrefresh: '',
+					contentnomore: ''
+				},
+				options: {
+					...enumConverter
+				}
+			}
+		},
+		onLoad(e) {
+			this._id = e.id
+		},
+		onReady() {
+			if (this._id) {
+				this.queryWhere = '_id=="' + this._id + '"'
+			}
+		},
+		methods: {
+			handleCart() {
+				// 打开修改页面
+				// uni.navigateTo({
+				// 	url: './edit?id=' + this._id,
+				// 	events: {
+				// 		// 监听修改页面成功修改数据后, 刷新当前页面数据
+				// 		refreshData: () => {
+				// 			this.$refs.udb.loadData({
+				// 				clear: true
+				// 			})
+				// 		}
+				// 	}
+				// })
+			},
+			handleBuy() {
+				// this.$refs.udb.remove(this._id, {
+				// 	success: (res) => {
+				// 		// 删除数据成功后跳转到list页面
+				// 		uni.navigateTo({
+				// 			url: './list'
+				// 		})
+				// 	}
+				// })
+			}
+		}
+	}
 </script>
 
 <style>
-  .container {
-    padding: 10px;
-  }
+	.btns {
+		padding: 40rpx;
+		display: flex;
+	}
 
-  .btns {
-    margin-top: 10px;
-    /* #ifndef APP-NVUE */
-    display: flex;
-    /* #endif */
-    flex-direction: row;
-  }
-
-  .btns button {
-    flex: 1;
-  }
-
-  .btn-delete {
-    margin-left: 10px;
-  }
+	.btns button {
+		width: 300rpx;
+	}
 </style>
