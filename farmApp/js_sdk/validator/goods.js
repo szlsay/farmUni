7f9b9c8cc990d99627ff25577dfd09f0ed0e2c7a
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-27 09:25:28 +0800

    app-goods

---
+++ farmApp/js_sdk/validator/goods.js
@@ -0,0 +1,191 @@
+// 表单校验规则由 schema2code 生成，不建议直接修改校验规则，而建议通过 schema2code 生成, 详情: https://uniapp.dcloud.net.cn/uniCloud/schema
+
+
+const validator = {
+  "goods_sn": {
+    "rules": [
+      {
+        "required": true
+      },
+      {
+        "format": "string"
+      }
+    ],
+    "title": "货号",
+    "label": "货号"
+  },
+  "name": {
+    "rules": [
+      {
+        "required": true
+      },
+      {
+        "format": "string"
+      }
+    ],
+    "title": "名称",
+    "label": "名称"
+  },
+  "keywords": {
+    "rules": [
+      {
+        "format": "string"
+      }
+    ],
+    "title": "关键字",
+    "label": "关键字"
+  },
+  "goods_desc": {
+    "rules": [
+      {
+        "format": "string"
+      }
+    ],
+    "title": "详细描述",
+    "label": "详细描述"
+  },
+  "goods_thumb": {
+    "rules": [
+      {
+        "format": "file"
+      }
+    ],
+    "title": "商品图片",
+    "label": "商品图片"
+  },
+  "remain_count": {
+    "rules": [
+      {
+        "required": true
+      },
+      {
+        "format": "int"
+      }
+    ],
+    "title": "库存数量",
+    "label": "库存数量"
+  },
+  "month_sell_count": {
+    "rules": [
+      {
+        "format": "int"
+      }
+    ],
+    "title": "月销量",
+    "defaultValue": 0,
+    "label": "月销量"
+  },
+  "total_sell_count": {
+    "rules": [
+      {
+        "format": "int"
+      }
+    ],
+    "title": "总销量",
+    "defaultValue": 0,
+    "label": "总销量"
+  },
+  "comment_count": {
+    "rules": [
+      {
+        "format": "int"
+      }
+    ],
+    "title": "评论数",
+    "defaultValue": 0,
+    "label": "评论数"
+  },
+  "is_on_sale": {
+    "rules": [
+      {
+        "format": "bool"
+      }
+    ],
+    "title": "是否上架",
+    "defaultValue": false,
+    "label": "是否上架"
+  },
+  "is_hot": {
+    "rules": [
+      {
+        "format": "bool"
+      }
+    ],
+    "title": "是否热销",
+    "defaultValue": false,
+    "label": "是否热销"
+  },
+  "add_date": {
+    "rules": [
+      {
+        "format": "timestamp"
+      }
+    ],
+    "title": "上架时间",
+    "defaultValue": {
+      "$env": "now"
+    },
+    "label": "上架时间"
+  },
+  "last_modify_date": {
+    "rules": [
+      {
+        "format": "timestamp"
+      }
+    ],
+    "title": "最后修改时间",
+    "defaultValue": {
+      "$env": "now"
+    },
+    "label": "最后修改时间"
+  }
+}
+
+const enumConverter = {}
+
+function filterToWhere(filter, command) {
+  let where = {}
+  for (let field in filter) {
+    let { type, value } = filter[field]
+    switch (type) {
+      case "search":
+        if (typeof value === 'string' && value.length) {
+          where[field] = new RegExp(value)
+        }
+        break;
+      case "select":
+        if (value.length) {
+          let selectValue = []
+          for (let s of value) {
+            selectValue.push(command.eq(s))
+          }
+          where[field] = command.or(selectValue)
+        }
+        break;
+      case "range":
+        if (value.length) {
+          let gt = value[0]
+          let lt = value[1]
+          where[field] = command.and([command.gte(gt), command.lte(lt)])
+        }
+        break;
+      case "date":
+        if (value.length) {
+          let [s, e] = value
+          let startDate = new Date(s)
+          let endDate = new Date(e)
+          where[field] = command.and([command.gte(startDate), command.lte(endDate)])
+        }
+        break;
+      case "timestamp":
+        if (value.length) {
+          let [startDate, endDate] = value
+          where[field] = command.and([command.gte(startDate), command.lte(endDate)])
+        }
+        break;
+    }
+  }
+  return where
+}
+
+export { validator, enumConverter, filterToWhere }
---
+++ farmApp/pages/goods/add.vue
@@ -0,0 +1,173 @@
+<template>
+  <view class="uni-container">
+    <uni-forms ref="form" :model="formData" validate-trigger="submit" err-show-type="toast">
+      <uni-forms-item name="goods_sn" label="货号" required>
+        <uni-easyinput placeholder="商品的唯一货号" v-model="formData.goods_sn" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="name" label="名称" required>
+        <uni-easyinput placeholder="商品名称" v-model="formData.name" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="keywords" label="关键字">
+        <uni-easyinput placeholder="商品关键字，为搜索引擎收录使用" v-model="formData.keywords" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="goods_desc" label="详细描述">
+        <uni-easyinput placeholder="商品详细描述" v-model="formData.goods_desc" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="goods_thumb" label="商品图片">
+        <uni-file-picker file-mediatype="image" file-extname="jpg,png" return-type="object" v-model="formData.goods_thumb"></uni-file-picker>
+      </uni-forms-item>
+      <uni-forms-item name="remain_count" label="库存数量" required>
+        <uni-easyinput placeholder="库存数量" type="number" v-model="formData.remain_count"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="month_sell_count" label="月销量">
+        <uni-easyinput placeholder="月销量" type="number" v-model="formData.month_sell_count"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="total_sell_count" label="总销量">
+        <uni-easyinput placeholder="总销量" type="number" v-model="formData.total_sell_count"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="comment_count" label="评论数">
+        <uni-easyinput placeholder="累计评论数" type="number" v-model="formData.comment_count"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="is_on_sale" label="是否上架">
+        <switch @change="binddata('is_on_sale', $event.detail.value)" :checked="formData.is_on_sale"></switch>
+      </uni-forms-item>
+      <uni-forms-item name="is_hot" label="是否热销">
+        <switch @change="binddata('is_hot', $event.detail.value)" :checked="formData.is_hot"></switch>
+      </uni-forms-item>
+      <uni-forms-item name="add_date" label="上架时间">
+        <uni-datetime-picker return-type="timestamp" v-model="formData.add_date"></uni-datetime-picker>
+      </uni-forms-item>
+      <uni-forms-item name="last_modify_date" label="最后修改时间">
+        <uni-datetime-picker return-type="timestamp" v-model="formData.last_modify_date"></uni-datetime-picker>
+      </uni-forms-item>
+      <view class="uni-button-group">
+        <button type="primary" class="uni-button" @click="submit">提交</button>
+      </view>
+    </uni-forms>
+  </view>
+</template>
+
+<script>
+  import { validator } from '../../js_sdk/validator/goods.js';
+
+  const db = uniCloud.database();
+  const dbCollectionName = 'goods';
+
+  function getValidator(fields) {
+    let result = {}
+    for (let key in validator) {
+      if (fields.indexOf(key) > -1) {
+        result[key] = validator[key]
+      }
+    }
+    return result
+  }
+
+  
+
+  export default {
+    data() {
+      let formData = {
+        "goods_sn": "",
+        "name": "",
+        "keywords": "",
+        "goods_desc": "",
+        "goods_thumb": null,
+        "remain_count": null,
+        "month_sell_count": 0,
+        "total_sell_count": 0,
+        "comment_count": 0,
+        "is_on_sale": false,
+        "is_hot": false,
+        "add_date": null,
+        "last_modify_date": null
+      }
+      return {
+        formData,
+        formOptions: {},
+        rules: {
+          ...getValidator(Object.keys(formData))
+        }
+      }
+    },
+    onReady() {
+      this.$refs.form.setRules(this.rules)
+    },
+    methods: {
+      
+      /**
+       * 验证表单并提交
+       */
+      submit() {
+        uni.showLoading({
+          mask: true
+        })
+        this.$refs.form.validate().then((res) => {
+          return this.submitForm(res)
+        }).catch(() => {
+        }).finally(() => {
+          uni.hideLoading()
+        })
+      },
+
+      /**
+       * 提交表单
+       */
+      submitForm(value) {
+        // 使用 clientDB 提交数据
+        return db.collection(dbCollectionName).add(value).then((res) => {
+          uni.showToast({
+            icon: 'none',
+            title: '新增成功'
+          })
+          this.getOpenerEventChannel().emit('refreshData')
+          setTimeout(() => uni.navigateBack(), 500)
+        }).catch((err) => {
+          uni.showModal({
+            content: err.message || '请求服务失败',
+            showCancel: false
+          })
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+  .uni-container {
+    padding: 15px;
+  }
+
+  .uni-input-border,
+  .uni-textarea-border {
+    width: 100%;
+    font-size: 14px;
+    color: #666;
+    border: 1px #e5e5e5 solid;
+    border-radius: 5px;
+    box-sizing: border-box;
+  }
+
+  .uni-input-border {
+    padding: 0 10px;
+    height: 35px;
+
+  }
+
+  .uni-textarea-border {
+    padding: 10px;
+    height: 80px;
+  }
+
+  .uni-button-group {
+    margin-top: 50px;
+    /* #ifndef APP-NVUE */
+    display: flex;
+    /* #endif */
+    justify-content: center;
+  }
+
+  .uni-button {
+    width: 184px;
+  }
+</style>
