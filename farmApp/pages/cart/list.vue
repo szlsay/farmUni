769fef56a404b0242ea3c039d6988c8b4ebcbe53
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-28 17:33:48 +0800

    udpate list

--- farmApp/pages/cart/list.vue
+++ farmApp/pages/cart/list.vue
@@ -1,72 +1,104 @@
 <template>
-  <view class="container">
-    <unicloud-db ref="udb" v-slot:default="{data, pagination, loading, hasMore, error}" :collection="collectionList" field="goods_id,qty">
-      <view v-if="error">{{error.message}}</view>
-      <view v-else-if="data">
-        <uni-list>
-          <uni-list-item v-for="(item, index) in data" :key="index" showArrow :clickable="true" @click="handleItemClick(item._id)">
-            <template v-slot:body>
-              <text>
-                <!-- 此处默认显示为_id，请根据需要自行修改为其他字段 -->
-                <!-- 如果使用了联表查询，请参考生成的 admin 项目中 list.vue 页面 -->
-                {{item._id}}
-              </text>
-            </template>
-          </uni-list-item>
-        </uni-list>
-      </view>
-      <uni-load-more :status="loading?'loading':(hasMore ? 'more' : 'noMore')"></uni-load-more>
-    </unicloud-db>
-    <uni-fab ref="fab" horizontal="right" vertical="bottom" :pop-menu="false" @fabClick="fabClick" />
-  </view>
+	<view class="container">
+		<uni-list v-for="item in list">
+			<uni-list-item>
+				<template v-slot:body>
+					<view class="body-content">
+						<view class="item-left">
+							<image :src="item.goods_thumb.path" mode="aspectFill" class="item-img" />
+						</view>
+						<view class="item-right">
+						</view>
+					</view>
+				</template>
+			</uni-list-item>
+		</uni-list>
+	</view>
 </template>
 
+<!-- :title="getOneData(item.name)" :note="String(item.qty)" :thumb="getOneData(item.imgurl)"
+				thumb-size='lg' -->
+				
+				
 <script>
-  const db = uniCloud.database()
-  export default {
-    data() {
-      return {
-        collectionList: "cart",
-        loadMore: {
-          contentdown: '',
-          contentrefresh: '',
-          contentnomore: ''
-        }
-      }
-    },
-    onPullDownRefresh() {
-      this.$refs.udb.loadData({
-        clear: true
-      }, () => {
-        uni.stopPullDownRefresh()
-      })
-    },
-    onReachBottom() {
-      this.$refs.udb.loadMore()
-    },
-    methods: {
-      handleItemClick(id) {
-        uni.navigateTo({
-          url: './detail?id=' + id
-        })
-      },
-      fabClick() {
-        // 打开新增页面
-        uni.navigateTo({
-          url: './add',
-          events: {
-            // 监听新增数据成功后, 刷新当前页面数据
-            refreshData: () => {
-              this.$refs.udb.loadData({
-                clear: true
-              })
-            }
-          }
-        })
-      }
-    }
-  }
+	const db = uniCloud.database()
+	export default {
+		data() {
+			return {
+				list: []
+				// collectionList: "cart",
+				// loadMore: {
+				//   contentdown: '',
+				//   contentrefresh: '',
+				//   contentnomore: ''
+				// }
+			}
+		},
+		onShow() {
+			// this.getData()
+		},
+
+		onPullDownRefresh() {
+			this.$refs.udb.loadData({
+				clear: true
+			}, () => {
+				uni.stopPullDownRefresh()
+			})
+		},
+		onReachBottom() {
+			this.$refs.udb.loadMore()
+		},
+		methods: {
+			getOneData(data) {
+				if (data && data.length > 0) {
+					return data[0]
+				} else {
+					return ''
+				}
+			},
+			getData() {
+				// db.collection('cart').get().then(res => {
+				// 	console.log(res);
+				// })
+				db.collection('cart,goods')
+					.field('goods_id.goods_thumb.path as imgurl, goods_id.name as name, qty')
+					// .field('goods_id{name,goods_thumb} as goods_info, qty')
+					.get()
+					.then(res => {
+						console.log(res);
+						this.list = res.result.data
+						// this.$refs.alertCode.open(res.result)
+					}).catch(err => {
+						console.error(err)
+					}).finally(() => {
+						uni.hideLoading()
+					})
+			},
+			handleItemClick(id) {
+				uni.navigateTo({
+					url: './detail?id=' + id
+				})
+			},
+			fabClick() {
+				// 打开新增页面
+				uni.navigateTo({
+					url: './add',
+					events: {
+						// 监听新增数据成功后, 刷新当前页面数据
+						refreshData: () => {
+							this.$refs.udb.loadData({
+								clear: true
+							})
+						}
+					}
+				})
+			}
+		}
+	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.body-content{
+		display: flex;
+	}
 </style>
